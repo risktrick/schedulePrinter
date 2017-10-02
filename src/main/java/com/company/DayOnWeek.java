91f26792f1f66a452603e40580f158c85a2bd924
user-lenovo <user@user.com>
2017-10-03 00:37:28 +0300

    thinking about put method

--- src/main/java/com/company/DayOnWeek.java
+++ src/main/java/com/company/DayOnWeek.java
@@ -36,6 +36,26 @@ public class DayOnWeek implements Comparable<DayOnWeek>{
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DayOnWeek)) return false;
+
+        DayOnWeek dayOnWeek = (DayOnWeek) o;
+
+        if (value != dayOnWeek.value) return false;
+        if (jsonCode != null ? !jsonCode.equals(dayOnWeek.jsonCode) : dayOnWeek.jsonCode != null) return false;
+        return name != null ? name.equals(dayOnWeek.name) : dayOnWeek.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = jsonCode != null ? jsonCode.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + value;
+        return result;
+    }
+
+    @Override
     public int compareTo(DayOnWeek o) {
         if (o.getValue() == this.getValue()) {
             return 0;
--- src/main/java/com/company/model/SchedulerModel.java
+++ src/main/java/com/company/model/SchedulerModel.java
@@ -1,37 +1,20 @@
 package com.company.model;
 
+import com.company.DayOnWeek;
+
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
 public class SchedulerModel {
-    public enum DAYS_OF_WEEK
-    {   MON("mon", "пн"),
-        TUE("tue", "вт"),
-        WED("wed", "ср"),
-        THU("thu", "чт"),
-        FRI("fri", "пт"),
-        SAT("sat", "сб"),
-        SUN("sun", "вс");
 
-        public String jsonCode;
-        public String name;
-        DAYS_OF_WEEK(String jsonCode, String name) {
-            this.jsonCode = jsonCode;
-            this.name = name;
+    private final TreeMap<DayOnWeek, DayModel> daysMap = new TreeMap<>();
+
+    public SchedulerModel(List<DayOnWeek> daysOnWeek) {
+        for (DayOnWeek dayOnWeek : daysOnWeek) {
+            daysMap.put(dayOnWeek, null);
         }
-    }
-
-    private final TreeMap<DAYS_OF_WEEK, DayModel> daysMap = new TreeMap<>();
-
-    public SchedulerModel() {
-        daysMap.put(DAYS_OF_WEEK.MON, null);
-        daysMap.put(DAYS_OF_WEEK.TUE, null);
-        daysMap.put(DAYS_OF_WEEK.WED, null);
-        daysMap.put(DAYS_OF_WEEK.THU, null);
-        daysMap.put(DAYS_OF_WEEK.FRI, null);
-        daysMap.put(DAYS_OF_WEEK.SAT, null);
-        daysMap.put(DAYS_OF_WEEK.SUN, null);
     }
 
     public void put(DAYS_OF_WEEK day, DayModel dayModel) {
